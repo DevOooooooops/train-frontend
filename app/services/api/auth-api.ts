ca8732ed1a639abf30d2e36cc94cfe3db31c6111
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-01-27 06:40:19 +0300

    chore: use base api

--- app/services/api/auth-api.ts
+++ app/services/api/auth-api.ts
@@ -1,28 +1,29 @@
-import axios from "axios"
-import { GetTokenResult, GetWhoAmIResult } from "app/services/api/api.types"
+import { GetTokenResult, GetWhoAmIResult } from 'app/services/api/api.types';
+import { apiBase } from './base';
 
 interface getTokenData {
   data: {
-    bearerToken: string
-    username: string
-  }
+    bearerToken: string;
+    username: string;
+  };
 }
 export class AuthApi {
-
   async getToken(username: string, password: string): Promise<GetTokenResult> {
-    const response: getTokenData = await axios.post('http://192.168.0.12:8080/token', {
+    const response: getTokenData = await apiBase.post('token', {
       username: username,
-      password: password
+      password: password,
     });
-    const accessToken = response.data.bearerToken
-    return { accessToken: accessToken } ;
+    const accessToken = response.data.bearerToken;
+    return { accessToken: accessToken };
   }
 
   async whoami(token: string): Promise<GetWhoAmIResult> {
-    const response = await axios.get('http://192.168.0.12:8080/whoami', {headers: {
-        'Authorization': `Bearer ${token}`
-      }});
-    const user = response.data
-    return { user: user } ;
+    const response = await apiBase.get('/whoami', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const user = response.data;
+    return { user: user };
   }
 }
